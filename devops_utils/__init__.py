4f407c713e38cb7427a6430d468982e8f69bd066
gimoh <gimoh@bitmessage.ch>
2015-03-26 15:18:37 +0000

    Add package scaffolding
    Prep to split docker-init.

---
+++ devops_utils/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 gimoh
+#
+# This file is part of devops-utils.
+#
+# devops-utils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# devops-utils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with devops-utils.  If not, see <http://www.gnu.org/licenses/>.
+
+"""devops-utils - devops-utils image helper tools"""
+
+__author__ = 'gimoh'
+__email__ = 'gimoh@bitmessage.ch'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 gimoh
+#
+# This file is part of devops-utils.
+#
+# devops-utils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# devops-utils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with devops-utils.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.md').read()
+
+setup(
+    name='devops-utils',
+    version='0.1.0',
+    description='devops-utils image helper tools',
+    long_description=readme,
+    author='gimoh',
+    author_email='gimoh@bitmessage.ch',
+    url='https://github.com/gimoh/devops-utils',
+    packages=[
+        'devops_utils',
+    ],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license='GPLv3',
+    keywords='devops-utils docker',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
